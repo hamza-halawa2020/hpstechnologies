83ad255a4ff24f631a2456ebbf53714defbfcc91
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-08-15 20:50:08 +0300

    up

--- angular-site/src/app/app.ts
+++ angular-site/src/app/app.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SiteHeader } from './layout/site-header/site-header';
-import { SiteFooter } from './layout/site-footer/site-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SiteHeader, SiteFooter],
+  imports: [RouterOutlet],
   template: `
-    <app-site-header />
     <router-outlet />
-    <app-site-footer />
   `,
 })
 export class App {
